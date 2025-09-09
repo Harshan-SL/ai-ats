8ae54786c807ee47f6fda18e3a683ce162be447d
Harshan-SL <harshan.bhkl@gmail.com>
2025-09-09 22:00:32 +0530

    till:navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "New project" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar/>
+      <section className="main-section">
+        <div className="page-heading">
+            <h1>ATS tracking</h1>
+            <h2>Resume ai powered feedback</h2>
+        </div>
+      </section>
+  </main>;
 }
