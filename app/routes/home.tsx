c00698f30d781cbf7154b63cc95a26722265c131
Harshan-SL <harshan.bhkl@gmail.com>
2025-09-09 22:55:43 +0530

    till:homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import {callbackify} from "node:util";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,10 +15,18 @@ export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar/>
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
             <h1>ATS tracking</h1>
             <h2>Resume ai powered feedback</h2>
         </div>
+
+      {resumes.length>0 &&(
+        <div className="resume-section">
+            {resumes.map((resume)=>(
+                <ResumeCard key={resume.id} resume={resume}></ResumeCard>
+            ))}
+        </div>
+      )}
       </section>
   </main>;
 }
